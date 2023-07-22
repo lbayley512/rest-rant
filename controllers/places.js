bc0d025e4f0132d1ea36ed08203adca703fa879a
lbayley512 <lbayley512@gmail.com>
2023-07-22 15:09:50 -0700

    adding places view

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,27 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    res.send('GET /places')
+    let places = [{
+        name: 'Jamba Juice',
+        city: 'Burbank',
+        state: 'WA',
+        cuisines: 'Smoothies',
+        pic: 'https://www.jamba.com/-/media/jamba/app-images/electric-berry-lemonade/ebl_smoothie_306x235.png?v=1&d=20210224T084917Z'
+    }, {
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      
+    res.render('places/index', { places })
 })
 
 module.exports = router
