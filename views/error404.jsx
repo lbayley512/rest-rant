e9f6c4506cc335f7eeb3841a76ac9448ad554149
lbayley512 <lbayley512@gmail.com>
2023-07-24 21:11:36 -0700

    adding more style/imgaes

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,6 +6,12 @@ function error404 () {
         <main>
             <h1>404: PAGE NOT FOUND</h1>
             <p> Oops, sorry, we cant find this page</p>
+            <div>
+                <img src="/images/404Page.jpg" alt="Judging dog"/>
+            </div>
+            <div>
+                Photo by <a href="https://unsplash.com/@charlesdeluvio">Charles Deluvio</a> on  <a href="https://unsplash.com/photos/MsTOg6rhRVk">Unsplash</a>
+            </div>
         </main>
     </Def>)
 }
--- views/home.jsx
+++ views/home.jsx
@@ -5,6 +5,12 @@ function home () {
    return ( <Def>
         <main>
             <h1>HOME</h1>
+            <div>
+                <img src="/images/drink.jpg" alt="Chia Shake"/>
+            </div>
+            <div>
+                Photo by <a href="https://unsplash.com/@cravethebenefits">Brenda Godinez</a> on  <a href="https://unsplash.com/photos/MsTOg6rhRVk">Unsplash</a>
+            </div>
             <a href="/places">
                 <button className="btn-primary">Places Page</button>
             </a>
