0bb20e369a59ea319fa5b2f84a896ccc65648fb7
lbayley512 <lbayley512@gmail.com>
2023-07-25 11:23:02 -0700

    adding post

--- controllers/places.js
+++ controllers/places.js
@@ -1,26 +1,7 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'Jamba Juice',
-        city: 'Burbank',
-        state: 'WA',
-        cuisines: 'Smoothies',
-        pic: 'https://www.jamba.com/-/media/jamba/app-images/electric-berry-lemonade/ebl_smoothie_306x235.png?v=1&d=20210224T084917Z'
-    }, {
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]
-      
     res.render('places/index', { places })
 })
 
@@ -30,7 +11,18 @@ router.get('/new', (req,res) => {
 
 router.post('/', (req,res) => {
   console.log(req.body)
-  res.send('POST/places')
+  if (!req.body.pic) {
+    // default image
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
 
 module.exports = router
---
+++ models/places.js
@@ -0,0 +1,19 @@
+module.exports = [{
+    name: 'Jamba Juice',
+    city: 'Burbank',
+    state: 'WA',
+    cuisines: 'Smoothies',
+    pic: 'https://www.jamba.com/-/media/jamba/app-images/electric-berry-lemonade/ebl_smoothie_306x235.png?v=1&d=20210224T084917Z'
+}, {
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/thai-place.jpg'
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/coffee-cat.jpg'
+}]
